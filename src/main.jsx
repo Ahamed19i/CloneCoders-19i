6cd8ad3279e726a43b42955518aa28035af0d9e9
Ahamed19i <168310951+Ahamed19i@users.noreply.github.com>
2024-09-03 17:44:26 +0000

    La strucutre complet du site

--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import Images from './components/Images.jsx'
-import Projet from './components/Projet.jsx'
+
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-    <Images />
-    <Projet />
   </React.StrictMode>,
 )
